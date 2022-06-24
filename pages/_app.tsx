1a0ff937336adf660507818258a2f99a250f7173
Гришин Петр Сергеевич <grishin_p@tass.ru>
2022-06-24 14:39:48 +0400

    Подключен redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from 'react';
 import Link from 'next/link';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
 import { Layout, Menu, Grid } from 'antd';
 import Sider from 'antd/lib/layout/Sider';
 import { Content, Footer } from 'antd/lib/layout/layout';
@@ -32,55 +34,60 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   if (isDashboard) {
     return (
-      <Layout hasSider>
-        {sm && (
-          <Sider
-            style={{
-              overflow: 'auto',
-              height: '100vh',
-              position: 'fixed',
-              left: 0,
-              top: 0,
-              bottom: 0,
-            }}
-          >
-            <div className={styles.logo}>
-              <WalletOutlined />
-              <span>Finance Manager</span>
-            </div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} />
-          </Sider>
-        )}
-        <Layout
-          className="site-layout"
-          style={{
-            marginLeft: sm ? 200 : 0,
-            minHeight: '100vh',
-          }}
-        >
-          <Content>
-            <div
-              className="site-layout-background"
-              style={{ padding: 24 }}
+      <Provider store={store}>
+        <Layout hasSider>
+          {sm && (
+            <Sider
+              style={{
+                overflow: 'auto',
+                height: '100vh',
+                position: 'fixed',
+                left: 0,
+                top: 0,
+                bottom: 0,
+              }}
             >
-              <Component {...pageProps} />
-            </div>
-          </Content>
-          <Footer
+              <div className={styles.logo}>
+                <WalletOutlined />
+                <span>Finance Manager</span>
+              </div>
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} />
+            </Sider>
+          )}
+          <Layout
+            className="site-layout"
             style={{
-              textAlign: 'center',
+              marginLeft: sm ? 200 : 0,
+              minHeight: '100vh',
             }}
           >
-            {`Finance Manager ©${new Date().getUTCFullYear()} Created by `}
-            <a href="https://github.com/nightlightmare">Petr Grishin</a>
-          </Footer>
+            <Content>
+              <div
+                className="site-layout-background"
+                style={{ padding: 24 }}
+              >
+                <Component {...pageProps} />
+              </div>
+            </Content>
+            <Footer
+              style={{
+                textAlign: 'center',
+              }}
+            >
+              {`Finance Manager ©${new Date().getUTCFullYear()} Created by `}
+              <a href="https://github.com/nightlightmare">Petr Grishin</a>
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
+      </Provider>
     );
   }
 
   return (
-    <Component {...pageProps} />
+    <Provider store={store}>
+      <Component {...pageProps} />
+
+    </Provider>
   );
 }
 
