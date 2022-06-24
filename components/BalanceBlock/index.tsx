4ea3430023b7eaf6e422df339ba8e2f7a1519d52
Гришин Петр Сергеевич <grishin_p@tass.ru>
2022-06-24 12:20:40 +0400

    Добавлена таблица с данными по компоненту

--- components/BalanceBlock/index.tsx
+++ components/BalanceBlock/index.tsx
@@ -1,66 +1,58 @@
-import { Card } from 'antd';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { Card, Table, Collapse } from 'antd';
 import Chart from 'react-google-charts';
 
+import styles from './BalanceBlock.module.scss';
+
 const BalanceBlock: React.FC = () => {
+  const [data] = useState([{
+    key: 1,
+    date: new Date('02-04-2022').toDateString(),
+    balance: 1220.06,
+  },
+  {
+    key: 2,
+    date: new Date('02-05-2022').toDateString(),
+    balance: 1210.46,
+  }]);
+
+  const columns = [
+    {
+      title: 'Дата',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Баланс',
+      dataIndex: 'balance',
+      key: 'balance',
+    },
+  ];
+
   const options = {
     legend: { position: 'none' },
     chartArea: { width: '80%', height: '80%' },
   };
 
-  const data = [
-    ['Date', 'Balance'],
-    ['02.03', 12],
-    ['03.03', 5.5],
-    ['04.03', 14],
-    ['05.03', 5],
-    ['06.03', 3.5],
-    ['07.03', 7],
-    ['08.03', 14],
-    ['09.03', 5],
-    ['10.03', 3.5],
-    ['11.03', 7],
-    ['02.03', 12],
-    ['03.03', 5.5],
-    ['04.03', 14],
-    ['05.03', 5],
-    ['06.03', 3.5],
-    ['07.03', 7],
-    ['08.03', 14],
-    ['09.03', 5],
-    ['10.03', 3.5],
-    ['11.03', 7],
-    ['02.03', 12],
-    ['03.03', 5.5],
-    ['04.03', 14],
-    ['05.03', 5],
-    ['06.03', 3.5],
-    ['07.03', 7],
-    ['08.03', 14],
-    ['09.03', 5],
-    ['10.03', 3.5],
-    ['11.03', 7],
-    ['02.03', 12],
-    ['03.03', 5.5],
-    ['04.03', 14],
-    ['05.03', 5],
-    ['06.03', 3.5],
-    ['07.03', 7],
-    ['08.03', 14],
-    ['09.03', 5],
-    ['10.03', 3.5],
-    ['11.03', 7],
-  ];
+  const chartData = useMemo(() => [['Date', 'Balance'], ...data.map((item) => [item.date, item.balance])], [data]);
 
   return (
     <Card title="Баланс" bordered>
       <Chart
         chartType="LineChart"
-        data={data}
+        data={chartData}
         options={options}
         width="100%"
         height={450}
       />
+
+      <div className={styles.table}>
+        <Collapse>
+          <Collapse.Panel header="Показать таблицу" key="1">
+            <Table columns={columns} dataSource={data} pagination={false} />
+          </Collapse.Panel>
+        </Collapse>
+      </div>
     </Card>
   );
 };
